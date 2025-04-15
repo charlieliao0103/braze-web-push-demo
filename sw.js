82c2f91acab7ec58694bd5af60a655b7fa7b6cee
charlieliao0103 <58993141+charlieliao0103@users.noreply.github.com>
2025-04-15 21:07:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,17 @@
-// Import Braze Service Worker first
 importScripts('https://js.appboycdn.com/web-sdk/5.8/service-worker.js');
 
-const CACHE_NAME = 'braze-sw-v1';
+const CACHE_NAME = 'braze-sw-v2';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(['/']))
-      .then(() => self.skipWaiting())
+      .then(cache => cache.addAll([
+        '/',
+        '/index.html'
+      ]))
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => 
-        key !== CACHE_NAME ? caches.delete(key) : null
-      ))
-    )
-  );
-});
-
-// Delegate to Braze's fetch handler
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.waitUntil(clients.claim());
 });
