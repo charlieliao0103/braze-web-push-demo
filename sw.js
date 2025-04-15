8323f7a33212047f6e150c89d2b6665678522f7d
charlieliao0103 <58993141+charlieliao0103@users.noreply.github.com>
2025-04-15 17:43:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-// Import Braze service worker
-importScripts('https://js.appboycdn.com/web-sdk/4.7/service-worker.js');
+const brazeConfig = {
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    userId: 'test-user-123', // Unique user identifier
+    enableLogging: true
+};
+
+const braze = require('@braze/web-sdk');
+braze.initialize(brazeConfig.apiKey, brazeConfig);
+
+// Start session tracking
+braze.openSession();
