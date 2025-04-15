38a083bed986308fc6abfe4f18cd11b3234580df
charlieliao0103 <58993141+charlieliao0103@users.noreply.github.com>
2025-04-15 17:26:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,2 @@
-// sw.js – Service Worker required for Braze Web Push
-
-self.addEventListener("push", function(event) {
-    if (event.data) {
-      const payload = event.data.json();
-      event.waitUntil(
-        self.registration.showNotification(payload.title, {
-          body: payload.body,
-          icon: payload.icon,
-          data: payload,
-        })
-      );
-    }
-  });
-  
-  self.addEventListener("notificationclick", function(event) {
-    event.notification.close();
-    const url = event.notification.data.url || "/";
-    event.waitUntil(clients.openWindow(url));
-  });
-  
+// Import Braze service worker
+importScripts('https://js.appboycdn.com/web-sdk/4.7/service-worker.js');
